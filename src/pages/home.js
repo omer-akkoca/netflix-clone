119345b0b583bd47924367cebe60930bb4fab97f
omer-akkoca <79708845+omer-akkoca@users.noreply.github.com>
2021-11-16 21:16:50 +0300

    pages

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import JumbotronContainer from '../containers/jumbotron';
+import FooterContainer from '../containers/footer';
+
+const Home = () => {
+    return (
+        <>
+            <JumbotronContainer/>
+            <FooterContainer/>
+        </>
+    )
+}
+
+export default Home;
